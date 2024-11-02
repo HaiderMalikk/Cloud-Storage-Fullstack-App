ae471a5bb8f9ca930bb16df372fca2b10814cfbd
HaiderMalikk <haidermalik662@gmail.com>
2024-11-01 22:24:37 -0400

    new

--- src/main/java/com/exampletest/demo/controller/usercontroller.java
+++ src/main/java/com/exampletest/demo/controller/usercontroller.java
@@ -3,14 +3,12 @@ package com.exampletest.demo.controller;
 import com.exampletest.demo.model.User;
 import com.exampletest.demo.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000") // for postman to react
 public class usercontroller {
 
     // injecting user
